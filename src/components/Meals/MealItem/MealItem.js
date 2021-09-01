ae3d58e4554cf9bbb9a51b59e5dd727e9913a1a8
Viktor Kushnir <mustbefail@gmail.com>
2021-09-01 21:27:21 +0300

    added meal item component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import styles from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
+import { identifier } from '@babel/types';
 
 const MealItem = ({ meal }) => {
-  const { name, description, price } = meal;
+  const cartCtx = useContext(CartContext);
+  const { name, description, price, id} = meal;
   const formatPrice = `$${price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id,
+      name,
+      price,
+      amount
+    })
+  };
   return (
     <li className={styles.meal}>
       <div>
@@ -11,7 +24,7 @@ const MealItem = ({ meal }) => {
         <div className={styles.description}>{description}</div>
         <div className={styles.price}>{formatPrice}</div>
       </div>
-      <MealItemForm />
+      <MealItemForm onAddToCart={addToCartHandler} />
     </li>
   );
 };
