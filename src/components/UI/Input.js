fd4f0f28744787572adee5f490946ea428c4cba8
Viktor Kushnir <mustbefail@gmail.com>
2021-09-01 21:28:41 +0300

    added input UI component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ inputSettings, label }) => {
+const Input = React.forwardRef(({ inputSettings, label }, ref) => {
   return (
     <div className={styles.input}>
       <label htmlFor={inputSettings.id}>{label}</label>
-      <input {...inputSettings} />
+      <input ref={ref} {...inputSettings} />
     </div>
   );
-};
+});
 
 export default Input;
