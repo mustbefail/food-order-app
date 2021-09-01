07c0d7fac4e84e9de056eee775fa5e5101d4178d
Viktor Kushnir <mustbefail@gmail.com>
2021-09-01 21:27:05 +0300

    added header and header cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import styles from './Header.module.css';
 import foodTabelImg from '../../assets/food-table.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = ({ onShowCart }) => {
   return (
     <>
       <header className={styles.header}>
         <h1>Food Order App</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onShowCart={onShowCart} />
       </header>
       <div className={styles['main-image']}>
         <img src={foodTabelImg} alt='A table full of food' />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = ({ onShowCart }) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce(
+    (acc, item) => acc + item.amount,
+    0
+  );
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={() => onShowCart()}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
